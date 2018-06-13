7ffbcc32c86b52d3b533b6d85560577fa716faa6
Carl Jokl <carljokl@Carls-MacBook-Pro-Retina.local>
2018-06-13 15:43:25 +0100

    Adding Android App app template

---
+++ app/src/main/java/com/bemyapp/hackxlr8/h2o/hackxlr8_h2o/H2OMainActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.bemyapp.hackxlr8.h2o.hackxlr8_h2o
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class H2OMainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_h2_omain, container, false)
+    }
+}
